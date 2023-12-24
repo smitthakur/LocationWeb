c921c9fd04223079a7acc028a1683d6b3208917c
smitthakur <smitthakur@gmail.com>
2023-12-24 13:13:20 +0530

    Location web project: Presentation layer demo 1st commit

---
+++ src/test/java/com/blackbucks/LocationWeb/LocationWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackbucks.LocationWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
